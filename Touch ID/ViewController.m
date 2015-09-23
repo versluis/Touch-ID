eb24c3ddd2799fa9a8148de35eecade9ff8d93c0
Jay Versluis <jay@versluis.com>
2015-09-23 12:38:06 -0400

    initial commit

--- Touch ID/ViewController.m
+++ Touch ID/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import LocalAuthentication;
 
 @interface ViewController ()
 
@@ -16,12 +17,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)authenticateTouchID {
+    
+    // create a context and error object
+    LAContext *context = [[LAContext alloc]init];
+    NSError *error = nil;
+    NSString *reason = @"We want to authenticate your fingerprints";
+    
+    // check if the device supports Touch ID
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        
+        NSLog(@"Device supports Touch ID");
+        
+        // authenticate the user
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:reason reply:^(BOOL success, NSError * _Nullable error) {
+            
+            // it worked!
+            if (success) {
+                NSLog(@"Successfully authenticated the user via Touch ID. HUZZAH!");
+            } else {
+                NSLog(@"Could not authenticate the user via Touch ID. \nReason: %@ \nError Code: %ld", error.localizedDescription, error.code);
+                
+                // if the user chooses the password option
+                if (error.code == -3) {
+                    // handle password input here
+                    NSLog(@"Let's ask for your password instead");
+                }
+            }
+        }];
+        
+    } else {
+        // Touch ID is not available
+        NSLog(@"Touch ID is not available.");
+    }
+}
+
+
+
 
 @end
